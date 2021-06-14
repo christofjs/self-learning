c9c2b898ac1f8c8f42b2ec1f4a4dabc9d4a2783d
christofjs <40004963+christofjs@users.noreply.github.com>
2021-06-14 02:45:12 -0800

    working on sparsify

--- mlexpert/matrix_multiply.py
+++ mlexpert/matrix_multiply.py
@@ -1,15 +1,20 @@
 def sparse_matrix_multiply(matrix_a, matrix_b):
     if len(matrix_a[0]) == len(matrix_b):
-        matrix_c = []
+        sparse_a_dict = sparsify(matrix_a)
+        sparse_b_dict = sparsify(matrix_b)
+        matrix_c = [[0] * len(matrix_a) for _ in range(len(matrix_b[0]))]
         for i in range(len(matrix_a)):
-            matrix_c.append([])
-            for j in range(len(matrix_b[0])):
-                matrix_c[i].append(0)
-                for k in range(len(matrix_a[0])):
+            for k in range(len(matrix_a[0])):
+                for j in range(len(matrix_b[0])):
                     matrix_c[i][j] += matrix_a[i][k] * matrix_b[k][j]
         return matrix_c
     else:
         return [[]]
+
+def sparsify(matrix):
+    for i in matrix:
+        for j in matrix[i]:
+            if matrix[i][j] != 0
 
 one = [[1,2],[3,4]]
 two = [[2,2],[2,2]]
