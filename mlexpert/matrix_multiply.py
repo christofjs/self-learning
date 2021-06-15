e2733fb2057168ba4391d6d2d1ff4498b84dec9b
christofjs <40004963+christofjs@users.noreply.github.com>
2021-06-15 09:02:39 -0800

    index out of range error

--- mlexpert/matrix_multiply.py
+++ mlexpert/matrix_multiply.py
@@ -3,18 +3,22 @@ def sparse_matrix_multiply(matrix_a, matrix_b):
         sparse_a_dict = sparsify(matrix_a)
         sparse_b_dict = sparsify(matrix_b)
         matrix_c = [[0] * len(matrix_a) for _ in range(len(matrix_b[0]))]
-        for i in range(len(matrix_a)):
-            for k in range(len(matrix_a[0])):
-                for j in range(len(matrix_b[0])):
-                    matrix_c[i][j] += matrix_a[i][k] * matrix_b[k][j]
+        for i,k in sparse_a_dict.keys():
+            for j in range(len(matrix_b[0])):
+                if (k,j) in sparse_b_dict.keys():
+                    matrix_c[i][j] += sparse_a_dict[(i,k)] * sparse_b_dict[(k,j)]
         return matrix_c
     else:
         return [[]]
 
 def sparsify(matrix):
-    for i in matrix:
-        for j in matrix[i]:
-            if matrix[i][j] != 0
+    sparsed_dict = {}
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            if matrix[i][j] != 0:
+                sparsed_dict[(i,j)] = matrix[i][j]
+    return sparsed_dict
+
 
 one = [[1,2],[3,4]]
 two = [[2,2],[2,2]]
